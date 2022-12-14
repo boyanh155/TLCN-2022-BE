b19e527f04178998abddbeceb902e599ca50df83
unknown <danbaonguyen2001@gmail.com>
2022-12-14 14:57:03 +0700

    đ

--- src/models/cart/item.js
+++ src/models/cart/item.js
@@ -1,62 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const ItemSchema = mongoose.Schema(
-  {
+
+const ItemSchema = mongoose.Schema({
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
     },
-    item: {
-      quantity: {
+    quantity: {
         type: Number,
         default: 1,
-        required: [true, "Missing quantity line-item"],
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      info: {
-        colorName: {
-          type: String,
-          required: true,
-        },
-        optionName: {
-          type: String,
-          required: true,
-        },
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      image: {
-        type: String,
-        required: true,
-      },
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Product",
-      },
-      color: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-      },
-      option: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-      },
-      countInStock: {
-        type: Number,
-        required: true,
-      },
+        required: [true, "Missing quantity line-item"]
     },
-  },
-  {
-    timestamps: true,
-  }
-);
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    }
+})
 
-const Item = mongoose.model("Item", ItemSchema);
-module.exports = Item;
+const Item = mongoose.model('Item', ItemSchema)
+
+module.exports = Item
+module.exports.schema = ItemSchema
